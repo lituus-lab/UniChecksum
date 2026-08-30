lituus-lab/UniCompress

fix(cabi): initialize the Nim runtime in static builds
A shared library runs NimMain from DllMain or an ELF constructor; a static
one has neither, so nothing initializes the runtime and the first call that
reads the environment faults. Only Windows links the static build from
Python, which is why this surfaced there alone.

Signed-off-by: Loïc Bartoletti <lbartoletti@lituus-lab.com>

src/UniCompress/c_api.nim
@@ -22,16 +22,38 @@ proc inputView(input: ptr UncheckedArray[byte]; inputLen: csize_t): seq[byte] =
   result = newSeq[byte](int(inputLen))
   copyMem(addr result[0], input, result.len)
 
+# A shared library runs NimMain from DllMain (Windows) or an ELF constructor;
+# a static one has neither, so nothing initializes the Nim runtime. Anything
+# that reads the environment then faults — proven on Windows, where the Python
+# extension is the one consumer that links the static build. The static-library
+# tasks pass -d:staticNoAutoInit; shared builds must not, or NimMain runs twice.
+when defined(staticNoAutoInit):
+  # A C static, not a Nim global: module initialization would reset a Nim one
+  # back to false and NimMain would run again on the next call. NimMain is
+  # declared here too — the generated prototype comes after this section.
+  {.emit: """/*VARSECTION*/
+void NimMain(void);
+static int ucmp_runtime_ready = 0;
+""".}
+  template ensureRuntime() =
+    {.emit: """
+  if (!ucmp_runtime_ready) { ucmp_runtime_ready = 1; NimMain(); }
+""".}
+else:
+  template ensureRuntime() = discard
+
 {.push exportc, cdecl, dynlib.}
 
 proc ucmp_deflate(input: ptr UncheckedArray[byte]; inputLen: csize_t;
     output: ptr UncheckedArray[byte]; capacity: csize_t): int64 {.raises: [].} =
+  ensureRuntime()
   try: copyResult(compress(inputView(input, inputLen)), output, capacity)
   except CatchableError, Defect: -1
 
 proc ucmp_inflate(input: ptr UncheckedArray[byte]; inputLen: csize_t;
     output: ptr UncheckedArray[byte]; capacity, maxOutput: csize_t): int64
     {.raises: [].} =
+  ensureRuntime()
   if maxOutput == 0 or maxOutput > csize_t(high(int64)): return -1
   try:
     copyResult(inflate(inputView(input, inputLen),
@@ -40,12 +62,14 @@ proc ucmp_inflate(input: ptr UncheckedArray[byte]; inputLen: csize_t;
 
 proc ucmp_zlib_deflate(input: ptr UncheckedArray[byte]; inputLen: csize_t;
     output: ptr UncheckedArray[byte]; capacity: csize_t): int64 {.raises: [].} =
+  ensureRuntime()
   try: copyResult(zlibDeflate(inputView(input, inputLen)), output, capacity)
   except CatchableError, Defect: -1
 
 proc ucmp_zlib_inflate(input: ptr UncheckedArray[byte]; inputLen: csize_t;
     output: ptr UncheckedArray[byte]; capacity, maxOutput: csize_t): int64
     {.raises: [].} =
+  ensureRuntime()
   if maxOutput == 0 or maxOutput > csize_t(high(int64)): return -1
   try:
     copyResult(zlibInflate(inputView(input, inputLen),

lituus-lab/UniChecksum

src/UniChecksum/c_api.nim
@@ -16,48 +16,78 @@ template foldSpan(state, data, length, updater: untyped): untyped =
     updater(state, bytes.toOpenArray(0, int(length) - 1))
 
 # Unmangled C symbols, C calling convention, exported from the shared lib.
+# A shared library runs NimMain from DllMain (Windows) or an ELF constructor;
+# a static one has neither, so nothing initializes the Nim runtime. Anything
+# that reads the environment then faults — proven on Windows, where the Python
+# extension is the one consumer that links the static build. The static-library
+# tasks pass -d:staticNoAutoInit; shared builds must not, or NimMain runs twice.
+when defined(staticNoAutoInit):
+  # A C static, not a Nim global: module initialization would reset a Nim one
+  # back to false and NimMain would run again on the next call. NimMain is
+  # declared here too — the generated prototype comes after this section.
+  {.emit: """/*VARSECTION*/
+void NimMain(void);
+static int unichecksum_runtime_ready = 0;
+""".}
+  template ensureRuntime() =
+    {.emit: """
+  if (!unichecksum_runtime_ready) { unichecksum_runtime_ready = 1; NimMain(); }
+""".}
+else:
+  template ensureRuntime() = discard
+
 {.push exportc, cdecl, dynlib.}
 
 proc unichecksum_crc32_init(): uint32 =
+  ensureRuntime()
   ## Starting CRC-32 state.
   crc32Init()
 
 proc unichecksum_crc32_update(state: uint32, data: ptr uint8,
     length: csize_t): uint32 =
+  ensureRuntime()
   ## Fold `length` bytes into a running CRC-32 state.
   foldSpan(state, data, length, crc32Update)
 
 proc unichecksum_crc32_final(state: uint32): uint32 =
+  ensureRuntime()
   ## Output transform turning a CRC-32 state into a checksum.
   crc32Final(state)
 
 proc unichecksum_crc32(data: ptr uint8, length: csize_t): uint32 =
+  ensureRuntime()
   ## CRC-32 of a whole span. Null or empty input is 0.
   crc32Final(foldSpan(crc32Init(), data, length, crc32Update))
 
 proc unichecksum_crc64_init(): uint64 =
+  ensureRuntime()
   ## Starting CRC-64/XZ state.
   crc64Init()
 
 proc unichecksum_crc64_update(state: uint64, data: ptr uint8,
     length: csize_t): uint64 =
+  ensureRuntime()
   ## Fold `length` bytes into a running CRC-64/XZ state.
   foldSpan(state, data, length, crc64Update)
 
 proc unichecksum_crc64_final(state: uint64): uint64 =
+  ensureRuntime()
   ## Output transform turning a CRC-64/XZ state into a checksum.
   crc64Final(state)
 
 proc unichecksum_crc64(data: ptr uint8, length: csize_t): uint64 =
+  ensureRuntime()
   ## CRC-64/XZ of a whole span. Null or empty input is 0.
   crc64Final(foldSpan(crc64Init(), data, length, crc64Update))
 
 proc unichecksum_adler32_init(): uint32 =
+  ensureRuntime()
   ## Starting Adler-32 state.
   adler32Init()
 
 proc unichecksum_adler32_update(state: uint32, data: ptr uint8,
     length: csize_t): uint32 =
+  ensureRuntime()
   ## Fold `length` bytes into a running Adler-32 state. A state whose halves
   ## are not both below the modulus is rejected: it cannot come from this API.
   if (state and 0xFFFF'u32) >= Adler32Modulus or (state shr 16) >= Adler32Modulus:
@@ -65,14 +95,17 @@ proc unichecksum_adler32_update(state: uint32, data: ptr uint8,
   foldSpan(state, data, length, adler32Update)
 
 proc unichecksum_adler32_final(state: uint32): uint32 =
+  ensureRuntime()
   ## Identity, kept so all three families share one Init/Update/Final shape.
   adler32Final(state)
 
 proc unichecksum_adler32(data: ptr uint8, length: csize_t): uint32 =
+  ensureRuntime()
   ## Adler-32 of a whole span. Null or empty input is 1.
   adler32Final(foldSpan(adler32Init(), data, length, adler32Update))
 
 proc unichecksum_version(): cstring =
+  ensureRuntime()
   ## Static version string; do not free.
   UniChecksumVersionC
 